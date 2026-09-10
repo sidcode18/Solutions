5f7ab74ad07e44c8acfb1183b0d4becd95919260
sidcode18 <sidharthsaji718@gmail.com>
2026-09-10 07:33:34 +0530

    Kadane 1

---
+++ Kadane's Algorithm/README.md
@@ -0,0 +1,5 @@
+## Points to remember
+
+- usually asked to find max or min of sum / or freq or start/end of a subarray (contiguous)
+- used instead of sliding window because it doesn't work for negative element
+- goal would be to find wht is asked at each index of the array and then best out of those is selected
---
+++ Kadane's Algorithm/max_sum_subarray.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size(), a, b ;
+        int best = nums[0], ans = nums[0] ;  // initially at index 0, best and ans is the element at start itself.
+        // from index 1, we have two options, either we can take the current element or we can take the current element and add it to the best sum we have found so far.
+        for(int i = 1; i < n; i++){
+            a = nums[i];
+            b = nums[i]+best;
+            best = max(a,b);
+            ans = max(ans,best);
+        }
+        return ans;
+    }
+};
